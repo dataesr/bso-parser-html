49737026226bb406834b4d3551ec7faec1985861
Anne L'Hôte <anne.lhote@gmail.com>
2021-07-09 10:45:44 +0200

    [fix] Fix error

--- project/server/main/global_parser.py
+++ project/server/main/global_parser.py
@@ -1,19 +1,22 @@
-import copy
 import json
+
 from bs4 import BeautifulSoup
-from project.server.main.doi_parser_mapping import MAPPING
-import project.server.main.parsers as parsers
-from project.server.main.logger import get_logger
 from jsonschema import exceptions, validate
-    
+
+from project.server.main.doi_parser_mapping import MAPPING
+from project.server.main.logger import get_logger
+import project.server.main.parsers as parsers
+
 schema = json.load(open('/src/project/server/main/schema.json', 'r'))
 
 logger = get_logger()
 
-def is_empty(res):
+
+def is_empty(res) -> bool:
     if len(res) <= 1:
         return True
     return False
+
 
 def validate_json_schema(datum: object, schema: dict) -> bool:
     is_valid = True
@@ -24,16 +27,16 @@ def validate_json_schema(datum: object, schema: dict) -> bool:
         logger.debug(error)
     return is_valid
 
+
 def need_parsing(html):
     if html[0:4] == "%PDF":
         return False
-
     if len(html) > 2000000 and ('<div' not in html):
         return False
-
     if html == 'file too large':
         return False
     return True
+
 
 def parse(doi, html, publisher=None):
     logger.debug(f"PARSER -- parsing {doi}")
@@ -43,8 +46,7 @@ def parse(doi, html, publisher=None):
         'sources': ['html']
     }
     if not need_parsing(html):
-        return res
-
+        return res_base
     exec = MAPPING.get(doi[0:7])
     soup = BeautifulSoup(html, 'lxml')
     res = {}
@@ -54,35 +56,29 @@ def parse(doi, html, publisher=None):
             res = exec.get('func')(soup, doi)
         except Exception as e:
             logger.debug(f"PARSER -- FAILED {e}")
-            # soup2 = BeautifulSoup(html, 'html.parser')
-            # res = exec.get('func')(soup2)
     res = apply_fallbacks(doi, soup, res)
     res = post_treat(doi, soup, res)
     res.update(res_base)
-    is_valid = validate_json_schema(datum = res, schema=schema)
+    is_valid = validate_json_schema(datum=res, schema=schema)
     if is_valid is False:
         logger.error(f"invalid schema for {doi}")
-        print(f"invalid schema for {doi}" + ttt, flush=True)
+        print(f"invalid schema for {doi}", flush=True)
     return res
+
 
 def apply_fallbacks(doi, soup, current_res):
     exec = MAPPING.get(doi[0:7], {})
     func_fallbacks = []
-    
     if "cbs" in exec:
         func_fallbacks += exec.get("cbs", []) 
     func_fallbacks += [parsers.parse_fallback_highwire, parsers.parse_fallback_dublincore, parsers.parse_fallback_tags]
-    
     for func in func_fallbacks:
         res = func(soup, doi)
         for field in res:
-            #print(field, flush=True)
-            #print(res[field], flush=True)
-            #print(current_res.get(field, ''), flush=True)
             if len(current_res.get(field, '')) == 0:
-                #print(f'replacing field {field} with fallback for doi {doi}', flush=True)
                 current_res[field] = res[field]
     return current_res
+
 
 def post_treat(doi, soup, current_res):
     authors = current_res.get('authors', [])
@@ -98,6 +94,5 @@ def post_treat(doi, soup, current_res):
     
     has_grant = current_res.get('has_grant', False)
     current_res['has_grant'] = has_grant
-
     return current_res
 
