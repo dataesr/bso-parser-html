b3f17ad22055735f03c94856ac578c7d51f01d00
Anne L'Hôte <anne.lhote@gmail.com>
2021-07-27 18:21:27 +0200

    [feat] Add exception_handler decorator for querying ES
    See https://github.com/dataesr/bso-publications/issues/9

--- project/server/main/tasks.py
+++ project/server/main/tasks.py
@@ -15,6 +15,18 @@ FRENCH_ALPHA2 = ['fr', 'gp', 'gf', 'mq', 're', 'yt', 'pm', 'mf', 'bl', 'wf', 'tf
 logger = get_logger(__name__)
 
 
+def exception_handler(func):
+    def inner_function(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except Exception as exception:
+            logger.error(f'{func.__name__} raises an error through decorator "exception_handler".')
+            logger.error(exception)
+            return None
+    return inner_function
+
+
+@exception_handler
 def get_matcher_results(publications: list, countries_to_keep: list) -> list:
     r = requests.post(matcher_endpoint_url, json={'publications': publications, 'countries_to_keep': countries_to_keep,
                                                   'queue': 'matcher_short'})
